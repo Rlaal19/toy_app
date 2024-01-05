9212fe077b9c27b93a705eff92b7435ef81a8db4
Rlaal19 <n.prarichaya23@gmail.com>
2024-01-05 18:58:26 +0700

    success

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :microposts
-    # validates FILL_IN, presence: True    
+    # validates FILL_IN, presence: true    
     # # Replace FILL_IN with the right code.
-    # validates FILL_IN, presence: True    
+    # validates FILL_IN, presence: true    
     # # Replace FILL_IN with the right code.
 
 
