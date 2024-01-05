c9f285e5b17564b98f91f499bdee515652ee93c1
Rlaal19 <n.prarichaya23@gmail.com>
2024-01-05 09:47:05 +0700

    not seccess

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_many :microposts
+    # validates FILL_IN, presence: True    
+    # # Replace FILL_IN with the right code.
+    # validates FILL_IN, presence: True    
+    # # Replace FILL_IN with the right code.
+
+
+end
